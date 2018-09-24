e4447adaa1be8a91f0be392c39d18ec999310fec
Logan Forman <35375036+TheKingDwarf@users.noreply.github.com>
2018-09-24 10:16:58 -0600

    Update finalDistance.py
    Try adding these print functions so you can see whats happening to x2 as you try and convert it

--- finalDistance.py
+++ finalDistance.py
@@ -4,8 +4,11 @@ y = symbols('y')
 def final_distance(x_2, function1, function2):
     distance = []
     for b in range (0, 100):
+        print(x_2[b])
         xtwo = x_2[b]
+        print(xtwo)
         x2 = int(xtwo)
+        print(x2)
         xone = b + 1
         x1 = int(xone)
         r = sqrt((x2 - x1)**2 + (function1.subs(y, x1) - function2.subs(y, x2))**2)
